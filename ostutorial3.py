087d7b9023a1aee130e6c91b8f8af9f5e9f991f7
oliviaosimon <42871888+oliviaosimon@users.noreply.github.com>
2018-09-25 11:59:19 -0400

    Updated from Brython Server: 9/25/2018 11:59:17 AM

--- ostutorial3.py
+++ ostutorial3.py
@@ -1,1 +1,8 @@
+#Olivia Simon
+#9.25.18
+#Assets & Looping
+from ggame import App, RectangleAsset, ImageAsset, SoundAsset
+from ggame import LineStyle, Color, Sprite, Sound
 
+myapp = App()
+myapp.run()
