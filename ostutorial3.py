9fc4d29aa58694479a7c319da501f15a3fec8a0d
oliviaosimon <42871888+oliviaosimon@users.noreply.github.com>
2018-09-26 11:15:30 -0400

    Updated from Brython Server: 9/26/2018 11:15:30 AM

--- ostutorial3.py
+++ ostutorial3.py
@@ -5,4 +5,13 @@ from ggame import App, RectangleAsset, ImageAsset, SoundAsset
 from ggame import LineStyle, Color, Sprite, Sound
 
 myapp = App()
+
+# define colors and line style
+green = Color(0x00ff00, 1)
+black = Color(0, 1)
+noline = LineStyle(0, black)
+# a rectangle asset and sprite to use as background
+bg_asset = RectangleAsset(myapp.width, myapp.height, noline, green)
+bg = Sprite(bg_asset, (0,0))
+
 myapp.run()
